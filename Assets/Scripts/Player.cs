88d8a2a1edda632520385744c10fccb9f4d5e8fb
eiji-ito <eiji.ito@gree.net>
2019-12-10 02:02:25 +0900

    add title scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -30,7 +31,9 @@ public class Player : MonoBehaviour
     void Start()
     {
         dashSpeed = DEFAULT_DASH_SPEED;
-        AudioManager.Instance.PlayBGM("bgm_maoudamashii_8bit14", 0.5f, true);
+
+        if (SceneManager.GetActiveScene().name != "TitleScene")
+            AudioManager.Instance.PlayBGM("bgm_maoudamashii_8bit14", 0.5f, true);
     }
 
     // Update is called once per frame
